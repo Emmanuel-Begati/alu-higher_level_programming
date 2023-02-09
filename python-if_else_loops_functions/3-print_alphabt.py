ce54f8491682d4ec6aea12c4c58923b83f5018b3
Emmanuel-Begati <e.yakubu@alustudent.com>
2023-02-08 17:38:27 -0800

    haha

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for item in range (97,123):
     if chr(item) != "q" and chr(item) != "r":
-        print ("{}".format (chr(item)), end="")
+        print ("{}".format(chr(item)), end="")
