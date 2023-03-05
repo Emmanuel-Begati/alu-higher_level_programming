b9976a212423d9d67c6ec7a5fb41b6caf7d26e95
Emmanuel-Begati <e.yakubu@alustudent.com>
2023-03-05 11:17:31 -0800

    this

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
+import sys
 from calculator_1 import add, sub, mul, div
-if __name__ == "__main__":
-    import sys
-    first_number =sys.argv[1]
-    second_number = sys.argv[3]
-    operator = sys.argv[2]
-    first_number = int(float(first_number))
-    second_number = int(float(second_number))
-    operator = str(operator)
-    if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py a operator b")
-        sys.exit(1)
-    if operator == "+":
-        print("{} + {} = {}".format(first_number, second_number, add(first_number, second_number)))
-    elif operator == "-":
-        print("{} - {} = {}".format(first_number, second_number, sub(first_number, second_number)))
-    elif operator == "*":
-        print("{} * {} = {}".format(first_number, second_number, mul(first_number, second_number)))
-    elif operator == "/":
-        print("{} / {} = {}".format(first_number, second_number, div(first_number, second_number)))
-    elif operator != "+" or "-" or "*" or "/":
-        print("Unknown operator. Only: +, -, * and / available")
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+
+if len(sys.argv) != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
+
+a = int(sys.argv[1])
+operator = sys.argv[2]
+b = int(sys.argv[3])
+
+if operator == "+":
+    result = add(a, b)
+elif operator == "-":
+    result = sub(a, b)
+elif operator == "*":
+    result = mul(a, b)
+elif operator == "/":
+    result = div(a, b)
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    sys.exit(1)
+
+print("{} {} {} = {}".format(a, operator, b, result))
