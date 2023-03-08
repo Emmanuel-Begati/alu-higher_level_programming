552a7fd800d99c0867d49fb907138f99bd62ba76
Emmanuel-Begati <e.yakubu@alustudent.com>
2023-03-08 07:58:44 -0800

    done

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_string = ""
-    for i not in ("c", "C"):
-        new_string += i
+    for i in my_string:
+        if i not in ("c", "C"):
+            new_string += i
     return new_string
 
